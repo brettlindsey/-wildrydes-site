c91c9d0aaee86794f3e84066802035cb23730d46
Brett Lindsey <brett.lindsey@me.com>
2024-12-09 16:58:09 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_eej3xdWWH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5omdchg93jvtledgpdh9lajqvc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
